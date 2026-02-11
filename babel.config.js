dfcaa5782c52631b9a0528b9215e921e7b28b7d7
Krishna <gupta.krishna.shree@gmail.com>
2026-02-11 19:51:59 +0545

    Final Commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '$assets': './src/assets',
+          '$redux': './src/redux',
+          '$screens': './src/screens',
+          '$constants': './src/constants',
+          '$helpers': './src/helpers',
+          '$components': './src/components',
+          '$hooks': './src/hooks',
+          '$navigation': './src/navigation',
+        },
+      },
+    ],
+    // Worklets plugin should usually come after the resolver
+    'react-native-worklets/plugin',
+  ],
 };
